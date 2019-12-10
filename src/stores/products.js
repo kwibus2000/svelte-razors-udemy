2c2899eda7b16ae41a71b119493014610e143bb2
Ad Rienks <adrienks@adrienks.nl>
2019-12-10 03:46:59 +0100

    112. Products Store

---
+++ src/stores/products.js
@@ -0,0 +1,32 @@
+import { writable, derived } from 'svelte/store';
+import localProducts from '../localProducts';
+import getProducts from '../strapi/getProducts';
+const store = writable([], () => {
+  setProducts();
+  return () => {};
+});
+
+async function setProducts() {
+  let products = await getProducts();
+  if (products) {
+    products = flattenProducts(products);
+    store.set(products);
+  }
+}
+
+// subscribe
+// set
+// update
+
+// flatten products
+function flattenProducts(data) {
+  return data.map(item => {
+    let image = item.image.url;
+    return { ...item, image };
+  });
+}
+// featured store
+export const featuredStore = derived(store, $featured => {
+  return $featured.filter(item => item.featured === true);
+});
+export default store;
